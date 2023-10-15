10059d3d8f96a58a19e2cbe0479181eae9785306
poovarasanflower21 <poovarasanking2104@gmail.com>
2023-10-15 12:15:17 +0530

    2 changes

--- python_assignments/python_assignment_2.py
+++ python_assignments/python_assignment_2.py
@@ -3,13 +3,15 @@ import psutil
 #count the number of cpu in machine
 cpu_count = psutil.cpu_count()
 print("Monitoring CPU usage...")
+def monitor():
+    try:
+        while True:
+            a= psutil.cpu_percent(cpu_count)
+            #start if loop when cpu performance greater than 80
+            if( a >= 8):
+                print(f"Alert! CPU usage exceeds threshold: {a}%")
+        #it will run continuously until the interrupt happens
+    finally: # once click Ctr+c it will gives a msg and end the process.
+        print("monitoring stoped")
 
-try:
-    while True:
-        a= psutil.cpu_percent(cpu_count)
-        #start if loop when cpu performance greater than 80
-        if( a >= 80):
-            print(f"Alert! CPU usage exceeds threshold: {a}%")
-    #it will run continuously until the interrupt happens
-finally: # once click Ctr+c it will gives a msg and end the process.
-    print("monitoring stoped")
+monitor()
